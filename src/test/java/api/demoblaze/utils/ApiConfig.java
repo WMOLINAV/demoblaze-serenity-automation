4fb952cc81178dded3a2757dcce95865ce82a660
Wilson Molina <wmiguel2009@hotmail.com>
2026-04-28 16:53:18 -0500

    Add intentional code smell to demonstrate SonarCloud detection

--- src/test/java/api/demoblaze/utils/ApiConfig.java
+++ src/test/java/api/demoblaze/utils/ApiConfig.java
@@ -7,5 +7,14 @@ public class ApiConfig {
     public static void configure() {
         RestAssured.baseURI = "https://api.demoblaze.com";
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+
+        public void doSomethingBad() {
+            if (true) {
+                System.out.println("This is a dummy code smell");
+            }
+        }
+
+
     }
 }
